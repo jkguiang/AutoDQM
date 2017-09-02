30905c070958034573173e44464b8a2c66d51fb6
Jonathan Guiang <jkguiang@gmail.com>
2017-09-01 20:48:27 -0700

    now reads only one, specified hist. Updated look of graph

--- AutoDQM.py
+++ AutoDQM.py
@@ -1,20 +1,27 @@
 import ROOT
 import os
-from tqdm import tqdm
+import sys
 
 # Parsing Functions -------
 
 # Scan 1D Hist, plot Pull hist and compute Chi^2
-def scan_1D(f_hist, r_hist):
+def scan_1D(f_hist, r_hist, hist, f_num):
     c = ROOT.TCanvas('c', 'Pull')
     ROOT.gStyle.SetOptStat(0)
     ROOT.gStyle.SetPalette(ROOT.kLightTemperature)
     ROOT.gStyle.SetNumberContours(255)
 
+    is_good = True
     chi2 = 0
+    is_outlier = False
+    max_pull = 0
     nBins = 0
     pull_hist = r_hist.Clone("pull_hist")
     pull_hist.Reset()
+
+    if f_hist.GetEntries() == 0:
+        is_good = False
+        return is_good, chi2, max_pull, is_outlier
 
     # Normalize f_hist
     f_hist.Scale(r_hist.GetEntries()/f_hist.GetEntries())
@@ -38,31 +45,48 @@ def scan_1D(f_hist, r_hist):
         # Sum pulls
         chi2 += new_pull**2
 
+        # Check for max pull
+        if new_pull > max_pull:
+            max_pull = new_pull
+
         # Fill Pull Histogram
         pull_hist.SetBinContent(x, new_pull)
 
     # Compute chi2
     chi2 = (chi2/nBins)
 
-    # Plot pull hist
-    pull_hist.SetFillColor(ROOT.kGreen)
-    pull_hist.SetLineColor(ROOT.kBlack)
-    pull_hist.SetTitle(pull_hist.GetTitle()+" Pull Values")
-    pull_hist.Draw("hist")
+    if chi2 > 80:
+
+        # Plot pull hist
+        pull_hist.SetFillColor(ROOT.kGreen)
+        pull_hist.SetLineColor(ROOT.kBlack)
+        pull_hist.SetTitle(pull_hist.GetTitle()+" Pull Values")
+        pull_hist.Draw("hist")
+
+        # Text box
+        text = ROOT.TLatex(0,.9,"#scale[0.6]{Run: "+f_num+"}");  
+        text.SetNDC(ROOT.kTRUE);
+        text.Draw()
+
+        is_outlier = True
+        c.SaveAs("plots/{0}_{1}.pdf".format(hist, f_num))
 
     # DEBUG
     # print("Chi2: {0}".format(chi2))
 
-    return chi2
+    return chi2, max_pull, is_outlier
 
 # Scan 2D Hist, plot Pull hist and compute Chi^2
-def scan_2D(f_hist, r_hist):
+def scan_2D(f_hist, r_hist, hist, f_num):
     c = ROOT.TCanvas('c', 'Pull')
     ROOT.gStyle.SetOptStat(0)
     ROOT.gStyle.SetPalette(ROOT.kLightTemperature)
     ROOT.gStyle.SetNumberContours(255)
 
+    is_good = True
     chi2 = 0
+    is_outlier = False
+    max_pull = 0
     nBins = 0
     pull_hist = r_hist.Clone("pull_hist")
     pull_hist.Reset()
@@ -70,6 +94,10 @@ def scan_2D(f_hist, r_hist):
     # DEBUG
     skip = 0
     overflow = 0
+
+    if f_hist.GetEntries() == 0:
+        is_good = False
+        return is_good, chi2, max_pull, is_outlier
 
     # Normalize f_hist
     f_hist.Scale(r_hist.GetEntries()/f_hist.GetEntries())
@@ -95,6 +123,10 @@ def scan_2D(f_hist, r_hist):
             # Sum pulls
             chi2 += new_pull**2
 
+            # Check if max_pull
+            if new_pull > max_pull:
+                max_pull = new_pull
+
             # Fill Pull Histogram
             if new_pull > 50:
                 overflow += 1
@@ -108,10 +140,20 @@ def scan_2D(f_hist, r_hist):
     # Compute chi2
     chi2 = (chi2/nBins)
 
-    # Plot pull hist
-    pull_hist.GetZaxis().SetRangeUser(-50, 50)
-    pull_hist.SetTitle(pull_hist.GetTitle()+" Pull Values")
-    pull_hist.Draw("colz")
+    if chi2 > 50:
+
+        # Plot pull hist
+        pull_hist.GetZaxis().SetRangeUser(-50, 50)
+        pull_hist.SetTitle(pull_hist.GetTitle()+" Pull Values")
+        pull_hist.Draw("colz")
+
+        # Text box
+        text = ROOT.TLatex(.79,.91,"#scale[0.6]{Run: "+f_num+"}");  
+        text.SetNDC(ROOT.kTRUE);
+
+        text.Draw()
+        is_outlier = True
+        c.SaveAs("plots/{0}_{1}.pdf".format(hist, f_num))
 
     # DEBUG
     # print("X Bins: {0}".format(r_hist.GetNbinsX()))
@@ -121,7 +163,7 @@ def scan_2D(f_hist, r_hist):
     # print("Capped data: {0}".format(overflow))
     # print("Chi2: {0}".format(chi2))
 
-    return chi2
+    return is_good, chi2, max_pull, is_outlier
 
 # End Parsing Functions ------
 
@@ -145,75 +187,130 @@ def auto_dqm():
 
     # Ensure no graphs are drawn to screen
     ROOT.gROOT.SetBatch(ROOT.kTRUE)
+    ROOT.gErrorIgnoreLevel = ROOT.kWarning
     # Set up canvas for chi2 plot
     C = ROOT.TCanvas('C', 'Chi2')
 
     # Location of root files
-    # tfiles_dir = "/nfs-6/userdata/bemarsh/CSC_DQM/Run2017/SingleMuon/"
-    # tfiles = os.listdir(tfiles_dir)
-    tfiles_dir = "/home/users/jguiang/projects/AutoDQM/test_files/"
+    tfiles_dir = "/nfs-6/userdata/bemarsh/CSC_DQM/Run2017/SingleMuon/"
     tfiles = os.listdir(tfiles_dir)
+    # tfiles_dir = "/home/users/jguiang/projects/AutoDQM/test_files/"
+    # tfiles = os.listdir(tfiles_dir)
 
-    # Main dir = location of plots, sub dir = sub-directories we are interested in
+    # Main dir = location of plots
     main_dir = "DQMData/Run {0}/CSC/Run summary/CSCOfflineMonitor/"
-    sub_dirs = ["Occupancy/", "recHits/"]
 
     # Reference file run number
     r_num = "301531"
 
     # Remove reference file from tfiles
-    if r_num in tfiles:
-        tfiles.remove(r_num+".root")
+    tfiles.remove(r_num+".root")
 
     # Chi2 Plot
-    chi2_hist = ROOT.TH1F("chi2", "#Chi^{2}", 20, 0, 100)
+    chi2_1D = ROOT.TH1F("chi2_1D", "#Chi^{2} for 1D Histograms", 20, 0, 100)
+    chi2_2D = ROOT.TH1F("chi2_2D", "#Chi^{2} for 2D Histograms", 20, 0, 100)
 
+    # PARSE SPECIFIC HISTOGRAM ------------
+    hist = "hORecHits"
+    sub_dir = "Occupancy/"
     # Parse root files
+    files = 0
+    outliers = 0
     for tfile in tfiles:
-        print "File: {0}".format(tfile)
+        # print "File: {0}".format(tfile)
+        files += 1
 
-        # Parse desired sub-directories
-        for sub_dir in sub_dirs:
-            hists = ROOT.TFile.Open(tfiles_dir+r_num+".root")
-            keys = hists.GetDirectory("{0}{1}".format(main_dir.format(r_num), sub_dir))
-            histograms = {}
+        f_num = tfile.split(".")[0]
 
-            # Populate histogram map
-            for key in keys.GetListOfKeys():
-                histograms[key.GetName()] = key.ReadObj()
+        # Open DQM root file
+        new_file = ROOT.TFile.Open(tfiles_dir+tfile)
 
-            # Parse histograms
-            for hist_name in histograms:
-                f_num = tfile.split(".")[0]
+        # Open reference file
+        new_ref = ROOT.TFile.Open(tfiles_dir+r_num+".root")
 
-                # Open DQM root file
-                new_file = ROOT.TFile.Open(tfiles_dir+tfile)
+        # Open recHits
+        f_path = "{0}{1}{2}".format(main_dir.format(f_num), sub_dir, hist)
+        r_path = "{0}{1}{2}".format(main_dir.format(r_num), sub_dir, hist)
+        # print("New File: "+f_path)
+        f_hist = new_file.Get(f_path)
 
-                # Open reference file
-                new_ref = ROOT.TFile.Open(tfiles_dir+r_num+".root")
+        # print("Reference: "+r_path)
+        r_hist = new_ref.Get(r_path)
+
+        if type(f_hist) == ROOT.TH1F:
+            is_good, chi2, max_pull, is_outlier = scan_1D(f_hist, r_hist, hist, f_num)
+            if is_good:
+                chi2_1D.Fill(chi2)
+            if is_outlier:
+                outliers += 1
+        elif type(f_hist) == ROOT.TH2F:
+            is_good, chi2, max_pull, is_outlier = scan_2D(f_hist, r_hist, hist, f_num)
+            if is_good:
+                chi2_2D.Fill(chi2)
+            if is_outlier:
+                outliers += 1
+        else:
+            print("Not a histogram: {0}".format(hist))
+
+        # Update Terminal
+        sys.stdout.write("\r")
+        sys.stdout.write("Files: {0}    Outliers: {1}".format(files, outliers)) 
+        sys.stdout.flush()
+
+    # PARSE ALL HISTOGRAMS ------------
+    # sub_dirs = ["Occupancy/"]
+    # Parse root files
+    # for tfile in tfiles:
+    #     print "File: {0}".format(tfile)
+
+    #     # Parse desired sub-directories
+    #     for sub_dir in sub_dirs:
+    #         hists = ROOT.TFile.Open(tfiles_dir+r_num+".root")
+    #         keys = hists.GetDirectory("{0}{1}".format(main_dir.format(r_num), sub_dir))
+    #         histograms = {}
+
+    #         # Populate histogram map
+    #         for key in keys.GetListOfKeys():
+    #             histograms[key.GetName()] = key.ReadObj()
+
+    #         # Parse histograms
+    #         for hist in histograms:
+    #             f_num = tfile.split(".")[0]
+
+    #             # Open DQM root file
+    #             new_file = ROOT.TFile.Open(tfiles_dir+tfile)
+
+    #             # Open reference file
+    #             new_ref = ROOT.TFile.Open(tfiles_dir+r_num+".root")
 
 
-                # Open recHits
-                f_path = "{0}{1}{2}".format(main_dir.format(f_num), sub_dir, hist_name)
-                r_path = "{0}{1}{2}".format(main_dir.format(r_num), sub_dir, hist_name)
-                # print("New File: "+f_path)
-                f_hist = new_file.Get(f_path)
+    #             # Open recHits
+    #             f_path = "{0}{1}{2}".format(main_dir.format(f_num), sub_dir, hist)
+    #             r_path = "{0}{1}{2}".format(main_dir.format(r_num), sub_dir, hist)
+    #             # print("New File: "+f_path)
+    #             f_hist = new_file.Get(f_path)
 
-                # print("Reference: "+r_path)
-                r_hist = new_ref.Get(r_path)
+    #             # print("Reference: "+r_path)
+    #             r_hist = new_ref.Get(r_path)
 
-                if "TH1" in str(histograms[hist_name]):
-                    chi2_hist.Fill(scan_1D(f_hist, r_hist))
-                elif "TH2" in str(histograms[hist_name]):
-                    chi2_hist.Fill(scan_2D(f_hist, r_hist))
-                else:
-                    print(histograms[hist_name])
+    #             if "TH1" in str(histograms[hist]):
+    #                 chi2_1D.Fill(scan_1D(f_hist, r_hist, hist, f_num))
+    #             elif "TH2" in str(histograms[hist]):
+    #                 chi2_2D.Fill(scan_2D(f_hist, r_hist, hist, f_num))
+    #             else:
+    #                 print(histograms[hist])
 
-    chi2_hist.GetXaxis().SetTitle("#Chi^{2}")
-    chi2_hist.GetYaxis().SetTitle("Entries")
-    chi2_hist.Draw("hist")
-    C.SaveAs("tests/test1.pdf")
+    chi2_1D.GetXaxis().SetTitle("#Chi^{2}")
+    chi2_1D.GetYaxis().SetTitle("Entries")
+    chi2_2D.GetXaxis().SetTitle("#Chi^{2}")
+    chi2_2D.GetYaxis().SetTitle("Entries")
+    chi2_1D.Draw("hist")
 
+    C.SaveAs("plots/chi2_1D.pdf")
+    chi2_2D.Draw("hist")
+    C.SaveAs("plots/chi2_2D.pdf")
+
+    print("\rFiles: {0}    Outliers: {1}\n".format(files, outliers))
 
 if __name__ == "__main__":
     auto_dqm()
