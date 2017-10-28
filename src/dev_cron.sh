b977b7e07298e2513ee3c46c1c666021aa156793
Jonathan Guiang <jkguiang@gmail.com>
2017-10-28 14:22:46 -0700

    removed xrd-based cron jobs

--- src/dev_cron.sh
+++
@@ -1,5 +0,0 @@
-if ! [[ -e /nfs-6/userdata/AutoDQM/fmap.json ]] ; then
-    touch /nfs-6/userdata/${USER}/AutoDQM/fmap.json
-fi
-
-python dev_cron.py '{"cron":"/SingleMuon/Run2017*PromptReco*/DQMIO"}'
