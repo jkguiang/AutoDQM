8a328ef8876ef6a54ba39df0f800a17b153ff918
Jonathan Guiang <jkguiang@gmail.com>
2018-02-10 23:20:21 +0100

    reformatting dir structure for CERN web hosting

--- setup.sh
+++
@@ -1,9 +0,0 @@
-main=${PWD}/src
-req_dirs=( ${main}/data ${main}/ref ${main}/pdfs ${main}/pngs ${main}/txts ${main}/temp )
-
-for dir in "${req_dirs[@]}" ; do
-    if ! [[ -e $dir ]] ; then
-        mkdir $dir
-        chmod -R 755 $dir
-    fi
-done
