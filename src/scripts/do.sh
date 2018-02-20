05cdbd3e96a36580ba50df71795b240f7407d32f
Jonathan Guiang <jkguiang@gmail.com>
2018-02-19 16:00:53 -0800

    updated for reformatted dir structure

--- src/scripts/do.sh
+++ src/scripts/do.sh
@@ -8,23 +8,24 @@
 
 cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_9_2_8; eval `/cvmfs/cms.cern.ch/common/scramv1 runtime -sh`; cd ~-
 
+pardir="$(dirname "$PWD")"
 if [[ "$1" == "retrieve_data" || "$1" == "retrieve_ref" || "$1" == "process" ]] ; then
     if [[ "$1" == "retrieve_data" ]] ; then
         shift
-        if [[ -e $PWD/data/$1 ]] ; then
-            rm $PWD/data/$1/*
+        if [[ -e $pardir/data/$1 ]] ; then
+            rm $pardir/data/$1/*
         else
-            mkdir $PWD/data/$1
-            chmod 755 -R $PWD/data/$1
+            mkdir $pardir/data/$1
+            chmod 755 -R $pardir/data/$1
         fi
     fi
     if [[ "$1" == "retrieve_ref" ]] ; then
         shift
-        if [[ -e $PWD/ref/$1 ]] ; then
-            rm $PWD/ref/$1/*
+        if [[ -e $pardir/ref/$1 ]] ; then
+            rm $pardir/ref/$1/*
         else
-            mkdir $PWD/ref/$1
-            chmod 755 -R $PWD/ref/$1
+            mkdir $pardir/ref/$1
+            chmod 755 -R $pardir/ref/$1
         fi
     fi
     if [[ "$1" == "process" ]] ; then
