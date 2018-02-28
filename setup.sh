c7ba557b255b580371f07b2e01bf42cd638c7700
Jonathan Guiang <jkguiang@gmail.com>
2018-02-27 18:04:26 -0800

    renamed main to main_dir

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-main=${PWD}/src
+main_dir=${PWD}/data
 
 # Build required directories
-req_dirs=( ${main}/data ${main}/ref ${main}/pdfs ${main}/pngs ${main}/txts ${main}/temp ${main}/scripts/database )
+req_dirs=( ${main_dir}/data ${main_dir}/ref ${main_dir}/pdfs ${main_dir}/pngs ${main_dir}/txts ${main_dir}/temp ${main_dir}/database )
 for dir in "${req_dirs[@]}" ; do
     if ! [[ -e $dir ]] ; then
         mkdir $dir
@@ -11,7 +11,7 @@ for dir in "${req_dirs[@]}" ; do
 done
 
 # Build database
-db=${main}/scripts/database
+db=${main_dir}/database
 new_db=$false
 db_dirs=( ${db}/Run2017 ${db}/Run2017/SingleMuon ${db}/Run2017/Cosmics )
 for dir in "${db_dirs[@]}" ; do
