192b4f9f50180714ca91ac2ffd7c9804e1549860
Jonathan Guiang <jkguiang@gmail.com>
2018-07-25 23:23:21 +0200

    adding events cut, using wbm instead of csc run info

--- autoref/autoref.py
+++ autoref/autoref.py
@@ -11,17 +11,17 @@ def autoref(data_run):
     else:
         with open("dqm.json", "r") as fin:
             dqm_map = json.load(fin)
-    if not os.path.isfile("lumi.json"):
-        lumi_map = retrieve.retrieve("lumi", table="runs", save=True)
+    if not os.path.isfile("wbm.json"):
+        wbm_map = retrieve.retrieve("wbm", table="runs", save=True)
     else:
-        with open("lumi.json", "r") as fin:
-            lumi_map = json.load(fin)
+        with open("wbm.json", "r") as fin:
+            wbm_map = json.load(fin)
 
     dqm_runs = dqm_map["runs"]
     dqm = dqm_map["data"]
-    lumi = lumi_map["data"]
+    wbm = wbm_map["data"]
 
-    ref_runs = get_runs.main("Run2018_Cosmics.txt")
+    ref_runs = get_runs.main("Run2018_SingleMuon.txt")
     ref_runs.sort()
 
     run_scan = range(0, ref_runs.index(data_run))
@@ -39,33 +39,35 @@ def autoref(data_run):
         if this_run not in dqm_runs: continue
         in_runreg = True
         if break_counter > 100: break
+        # Get available data from wbm.json
+        lumi_ratio, Nevents = get_wbm_data(data_run, this_run, wbm)
+        # Zeroth order: High enough stats
+        if Nevents and Nevents < 100000: continue
         # First order: recency
         if not best_ref:
             if dqm[str(this_run)]["RDA_CMP_OCCUPANCY"] == "GOOD":
                 best_ref = this_run
-                lumi_ratio = get_lumi_ratio(data_run, this_run, lumi) 
                 if lumi_ratio: best_ratio = lumi_ratio
-                O1_ref[this_run] = lumi_ratio
+                O1_ref[this_run] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents}
         # Second order: luminocity
         else:
             if dqm[str(this_run)]["RDA_CMP_OCCUPANCY"] != "GOOD": continue
             else:
-                lumi_ratio = get_lumi_ratio(data_run, this_run, lumi)
                 if not lumi_ratio: continue
                 if lumi_ratio > 0.68 and lumi_ratio < 1.32:
                     if best_ratio:
                         if lumi_ratio > best_ratio and lumi_ratio < 1:
                             best_ref = this_run
                             best_ratio = lumi_ratio
-                            O2_refs[str(this_run)] = lumi_ratio
+                            O2_refs[str(this_run)] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents}
                         elif lumi_ratio > 1 and lumi_ratio < best_ratio:
                             best_ref = this_run
                             best_ratio = lumi_ratio
-                            O2_refs[str(this_run)] = lumi_ratio
+                            O2_refs[str(this_run)] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents}
                     else:
                         best_ref = this_run
                         best_ratio = lumi_ratio
-                        O2_refs[str(this_run)] = lumi_ratio
+                        O2_refs[str(this_run)] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents}
 
         break_counter += 1
 
@@ -80,26 +82,29 @@ def autoref(data_run):
 
     return
 
-def get_lumi_ratio(data_run, this_run, lumi):
+def get_wbm_data(data_run, this_run, wbm):
 
-    if str(data_run) not in lumi: return None
-    elif str(this_run) not in lumi: return None
+    if str(data_run) not in wbm: return None, None
+    elif str(this_run) not in wbm: return None, None
+
+    Ntriggers = wbm[str(this_run)]["TRIGGERS"]
+    Nevents = (0.0028725994131)*Ntriggers+128324.464261 #from numpy fit done on uaf
 
     try:
-        data_avg_lumi = (float(lumi[str(data_run)]["INITLUMI"])+float(lumi[str(data_run)]["ENDLUMI"]))/2
-        if data_avg_lumi == 0: return None
-        this_avg_lumi = (float(lumi[str(this_run)]["INITLUMI"])+float(lumi[str(this_run)]["ENDLUMI"]))/2
-        if this_avg_lumi == 0: return None
+        data_avg_lumi = (float(wbm[str(data_run)]["INITLUMI"])+float(wbm[str(data_run)]["ENDLUMI"]))/2
+        if data_avg_lumi == 0: return None, Nevents
+        this_avg_lumi = (float(wbm[str(this_run)]["INITLUMI"])+float(wbm[str(this_run)]["ENDLUMI"]))/2
+        if this_avg_lumi == 0: return None, Nevents
     except TypeError: 
-        return None
+        return None, Nevents
 
     lumi_ratio = data_avg_lumi/this_avg_lumi
 
-    return lumi_ratio
+    return lumi_ratio, Nevents
 
 
 if __name__ == "__main__":
-    runs = get_runs.main("Run2018_Cosmics.txt")
+    runs = get_runs.main("Run2018_SingleMuon.txt")
     #import random
     #data_run = (runs[random.randint(0,len(runs)-1)])
     for data_run in runs:
