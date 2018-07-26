9e32a13a4005d679e080398300c32594638f6f44
Jonathan Guiang <jkguiang@gmail.com>
2018-07-26 21:56:05 +0200

    now uses start times, optimized output

--- autoref/ref.py
+++ autoref/ref.py
@@ -1,4 +1,6 @@
 import os
+from time import mktime
+from datetime import datetime
 import ROOT
 import json
 
@@ -23,8 +25,7 @@ def fetch_ref(data_run, ref_runs):
     run_scan = range(0, ref_runs.index(data_run))
     run_scan.reverse()
 
-    O1_ref = {} 
-    O2_refs = {} 
+    refs = {}
     best_ref = None
     best_ratio = None
     break_counter = 0 
@@ -36,7 +37,7 @@ def fetch_ref(data_run, ref_runs):
         in_runreg = True
         if break_counter > 100: break
         # Get available data from wbm.json
-        lumi_ratio, Nevents = _get_wbm_data(data_run, this_run, wbm)
+        lumi_ratio, Nevents, delta_t = _get_wbm_data(data_run, this_run, wbm)
         # Zeroth order: High enough stats
         if Nevents and Nevents < 100000: continue
         # First order: recency
@@ -44,7 +45,7 @@ def fetch_ref(data_run, ref_runs):
             if dqm[str(this_run)]["RDA_CMP_OCCUPANCY"] == "GOOD":
                 best_ref = this_run
                 if lumi_ratio: best_ratio = lumi_ratio
-                O1_ref[this_run] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents}
+                refs[this_run] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents, "delta_t":delta_t, "order":1, "best":False}
         # Second order: luminocity
         else:
             if dqm[str(this_run)]["RDA_CMP_OCCUPANCY"] != "GOOD": continue
@@ -55,47 +56,66 @@ def fetch_ref(data_run, ref_runs):
                         if lumi_ratio > best_ratio and lumi_ratio < 1:
                             best_ref = this_run
                             best_ratio = lumi_ratio
-                            O2_refs[str(this_run)] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents}
+                            refs[str(this_run)] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents, "delta_t":delta_t, "order":2, "best":False}
                         elif lumi_ratio > 1 and lumi_ratio < best_ratio:
                             best_ref = this_run
                             best_ratio = lumi_ratio
-                            O2_refs[str(this_run)] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents}
+                            refs[str(this_run)] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents, "delta_t":delta_t, "order":2, "best":False}
                     else:
                         best_ref = this_run
                         best_ratio = lumi_ratio
-                        O2_refs[str(this_run)] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents}
+                        refs[str(this_run)] = {"lumi_ratio":lumi_ratio, "Nevents":Nevents, "delta_t":delta_t, "order":2, "best":False}
 
         break_counter += 1
 
     if in_runreg:
-        refs = {"O1":O1_ref, "O2":O2_refs, "best":best_ref}
+        refs[best_ref]["best"] = True
         return refs
     else:
         return None
 
 def _get_wbm_data(data_run, this_run, wbm):
 
+    # Veto runs missing from WBM database
     if str(data_run) not in wbm: return None, None
     elif str(this_run) not in wbm: return None, None
 
+    # Get approximate gauge of available statistics
     Ntriggers = wbm[str(this_run)]["TRIGGERS"]
-    Nevents = (0.0028725994131)*Ntriggers+128324.464261 #from numpy fit done on uaf
+    Nevents = (0.0028725994131)*Ntriggers+128324.464261 # from numpy fit done on uaf
 
+    # Get run start time, time between runs
+    data_start = wbm[str(data_run)]["STARTTIME"]
+    this_start = wbm[str(this_run)]["STARTTIME"]
+
+    data_dt = datetime.strptime(data_start, "%Y-%m-%d %H:%M:%S")
+    this_dt = datetime.strptime(this_start, "%Y-%m-%d %H:%M:%S")
+
+    delta_dt = data_dt - this_dt
+    # Difference in start times in [days, hours, minutes, total(in seconds)]
+    delta_t = {"days": delta_dt.days,
+               "hours": delta_dt.seconds//3600,
+               "minutes": (delta_dt.seconds//60)%60,
+               "total": delta_dt.total_seconds()}
+
+    # Get luminosity ratio
     try:
         data_init_lumi = float(wbm[str(data_run)]["INITLUMI"])
         data_end_lumi = float(wbm[str(data_run)]["ENDLUMI"])
         data_avg_lumi = _get_avg_lumi(data_init_lumi, data_end_lumi)
-        if data_avg_lumi == 0: return None, Nevents
         this_init_lumi = float(wbm[str(this_run)]["INITLUMI"])
         this_end_lumi = float(wbm[str(this_run)]["ENDLUMI"])
         this_avg_lumi = _get_avg_lumi(this_init_lumi, this_end_lumi)
-        if this_avg_lumi == 0: return None, Nevents
     except TypeError: 
-        return None, Nevents
+        data_avg_lumi = 0
+        this_avg_lumi = 0
 
-    lumi_ratio = data_avg_lumi/this_avg_lumi
+    if this_avg_lumi > 0:
+        lumi_ratio = data_avg_lumi/this_avg_lumi
+    else:
+        lumi_ratio = None
 
-    return lumi_ratio, Nevents
+    return lumi_ratio, Nevents, delta_t
 
 def _get_avg_lumi(init_lumi, end_lumi):
 
